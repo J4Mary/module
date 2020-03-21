9c877f910e10ec4e713b608942e0ef7a4e342f3a
J4Mary <masha.pichienko@gmail.com>
2020-03-21 13:16:55 +0200

    Update version3.py

--- version3.py
+++ version3.py
@@ -8,10 +8,11 @@ def my_sum(list_of_numbers):
         s+=i
     return s
 
-l=string.split()
-    l1=[word[0:6]+'*' if len(word)>6 else word for word in l]
-    s=' '.join(l1)
-    return s
+def shortener(string):
+    l=string.split()
+        l1=[word[0:6]+'*' if len(word)>6 else word for word in l]
+        s=' '.join(l1)
+        return s
 
 def compare_ends(words):
     l1=[word for word in words if len(word)>=2 and word[0]==word[-1]]
